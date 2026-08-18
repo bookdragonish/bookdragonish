adefbcb6c7baf7cf223204d700b6446cad9866a9
Ingvild Sandven <ingviksa@stud.ntnu.no>
2026-08-18 23:31:30 +0200

    Styling the highlight component (#15)

--- src/components/HighlightComponent/HighlightComponent.tsx
+++ src/components/HighlightComponent/HighlightComponent.tsx
@@ -1,31 +1,49 @@
 import style from "./HighlightComponent.module.css";
 
 const highlights = [
-  "I have a Bachelor's in Informatics from NTNU and am currently pursuing a Master's in Informatics",
+  <>
+    I have a Bachelor's in <span>Informatics from NTNU</span> and am currently
+    pursuing a <span>Master's in Informatics</span>
+  </>,
 
-  "In the summer of 2026, I worked as a Full-Stack Developer intern at the startup Social Minds",
+  <>
+    In the summer of 2026, I worked as a{" "}
+    <span>Full-Stack Developer intern</span> at the startup Social Minds
+  </>,
 
-  "I have experience developing both university projects and independent applications from idea to deployment",
+  <>
+    I have experience developing both <span>university projects</span> and{" "}
+    <span>independent applications</span> from idea to deployment
+  </>,
 
-  "I developed and evaluated AI integrations, including Gemini and NorLLM, as part of my bachelor's project",
+  <>
+    I developed and evaluated <span>AI integrations</span>, including Gemini and
+    NorLLM, as part of my bachelor's project
+  </>,
 
-  "I thrive in team environments, and many of my projects are built in collaboration with others",
+  <>
+    I thrive in <span>team environments</span>, and many of my projects are
+    built in collaboration with others
+  </>,
 
-  "I am especially interested in software development, UI/UX, and building practical applications",
+  <>
+    I am especially interested in <span>software development</span>,{" "}
+    <span>UI/UX</span>, and building practical applications
+  </>,
 ];
 
 function HighlightComponent() {
   return (
     <section className={style.highlight_section}>
-      <h2 className={style.highlight_title}>Highlights</h2>
-      <p>
-        These keypoint summarize my page, if you do not have time to read it
-        all!
-      </p>
+      <article className={style.highlight_title_container}>
+        <span className={style.line}></span>
+        <h2 className={style.highlight_title}>Highlights</h2>
+      </article>
+      <p>These keypoint summarize my page, if you have limited time!</p>
 
       <ul>
-        {highlights.map((point) => (
-          <li>{point}</li>
+        {highlights.map((point, index) => (
+          <li key={index}>{point}</li>
         ))}
       </ul>
     </section>
